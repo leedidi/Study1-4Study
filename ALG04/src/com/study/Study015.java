025d8f48534b87a09ba15544067b5e3c45269166
leedidi <edy9527@gmail.com>
2021-10-03 18:05:22 +0900

    10/3 Study

---
+++ ALG04/src/com/study/Study015.java
@@ -0,0 +1,72 @@
+/*====================================
+  ◆◆◆ PART 4. 정렬 알고리즘 ◆◆◆ 
+ =====================================*/
+// 3. 퀵 정렬 소스코드
+
+// 3-1. 자바 코드
+package com.study;
+import java.util.*;
+
+public class Study015 {
+
+    public static void quickSort(int[] arr, int start, int end) {
+        if (start >= end) return; // 원소가 1개인 경우 종료
+        int pivot = start; // 피벗은 첫 번째 원소
+        int left = start + 1;
+        int right = end;
+        while (left <= right) {
+            // 피벗보다 큰 데이터를 찾을 때까지 반복
+            while (left <= end && arr[left] <= arr[pivot]) left++;
+            // 피벗보다 작은 데이터를 찾을 때까지 반복
+            while (right > start && arr[right] >= arr[pivot]) right--;
+            // 엇갈렸다면 작은 데이터와 피벗을 교체
+            if (left > right) {
+                int temp = arr[pivot];
+                arr[pivot] = arr[right];
+                arr[right] = temp;
+            }
+            // 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+        // 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+        quickSort(arr, start, right - 1);
+        quickSort(arr, right + 1, end);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        quickSort(arr, 0, n - 1);
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
+
+// 3-2. 파이썬의 장점을 살린 파이썬 코드
+/*
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0] # 피벗은 첫 번째 원소
+    tail = array[1:] # 피벗을 제외한 리스트
+
+    left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬을 수행하고, 전체 리스트를 반환
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
+*/
